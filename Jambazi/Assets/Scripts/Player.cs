576e4be1c5e88356f0de3a3321bbfd55e012bd60
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-03 05:35:19 +0300

    defining player's movement in c# class

--- Jambazi/Assets/Scripts/Player.cs
+++ Jambazi/Assets/Scripts/Player.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed;
+    private RigidBody2D rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<RigidBody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
     }
 }
